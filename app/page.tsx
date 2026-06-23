7a4f81efa055cec2bccf2db24509a314658a1acb
Dana <dmjohns18@gmail.com>
2026-06-23 14:35:00 -0400

    Restore correct homepage from June 5 deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import { redirect } from 'next/navigation'
-
+export const dynamic = 'force-static'
+import { readFileSync } from 'fs'
+import path from 'path'
 export default function Home() {
-  redirect('/home')
+  const html = readFileSync(path.join(process.cwd(), 'public', 'index.html'), 'utf8')
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
 }
