5c4f26f2c598991a2cd002fd93f8ea7178321536
Dana <dmjohns18@gmail.com>
2026-06-25 00:11:15 -0400

    Revert "extract body content only"
    This reverts commit 4dcad17f516563f8e6a47055a06a04aed85f40ce.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 export const dynamic = 'force-static'
 import { readFileSync } from 'fs'
 import path from 'path'
-
 export default function Home() {
   const html = readFileSync(path.join(process.cwd(), 'public', 'index.html'), 'utf8')
-  const bodyMatch = html.match(/<body[^>]*>([\s\S]*)<\/body>/i)
-  const body = bodyMatch ? bodyMatch[1] : html
-  return <div dangerouslySetInnerHTML={{ __html: body }} />
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
 }
