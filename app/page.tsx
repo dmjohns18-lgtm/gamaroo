299d0830d89f6dbc8f9cc98136db3ce6cf119254
Dana <dmjohns18@gmail.com>
2026-06-24 23:25:43 -0400

    remove page.tsx to fix route conflict

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = 'force-static'
-import { readFileSync } from 'fs'
-import path from 'path'
-export default function Home() {
-  const html = readFileSync(path.join(process.cwd(), 'public', 'index.html'), 'utf8')
-  return <div dangerouslySetInnerHTML={{ __html: html }} />
-}
- 
