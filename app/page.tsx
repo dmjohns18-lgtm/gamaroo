4dcad17f516563f8e6a47055a06a04aed85f40ce
Dana <dmjohns18@gmail.com>
2026-06-25 00:08:17 -0400

    extract body content only

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export const dynamic = 'force-static'
 import { readFileSync } from 'fs'
 import path from 'path'
+
 export default function Home() {
   const html = readFileSync(path.join(process.cwd(), 'public', 'index.html'), 'utf8')
-  return <div dangerouslySetInnerHTML={{ __html: html }} />
+  const bodyMatch = html.match(/<body[^>]*>([\s\S]*)<\/body>/i)
+  const body = bodyMatch ? bodyMatch[1] : html
+  return <div dangerouslySetInnerHTML={{ __html: body }} />
 }
